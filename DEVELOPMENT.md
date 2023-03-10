ecd9233d3c208ffce6d482d3171b45f4058a415b
Anish Gupta <anish@Anishs-MacBook-Air.local>
2023-03-11 00:25:20 +0530

    Randomly generate snake head on canvas

---
+++ DEVELOPMENT.md
@@ -0,0 +1,29 @@
+# Snake Game
+
+## Create empty body
+Completed, using CSS Grid
+
+## create a board
+completed using CSS Grid
+
+## Divide board into columns and rows
+Completed using CSS Grid and TS
+
+## display snake head 
+Completed, using random functions, and accessing grid
+
+## move snake head periodically
+
+## control snake head with arrow keys
+
+## create food randomly
+
+## detect when it reaches food
+
+## add snake tail
+
+## configure snake movement
+
+## increase size of snake
+
+## show score tracker
--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,45 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
 
-let my_canvas = document.getElementById('canvas') as HTMLCanvasElement
-console.log(my_canvas.width)
+const NUMBER_OF_CELLS = 24
+
+let board = document.getElementById('board')
+if(board != null) initialiseBoard(board)
+
+setSnakeHead()
+
+
+function initialiseBoard(board: HTMLElement){
+  console.log("BOARD INITIALISING...")
+  for (let i = 0; i < NUMBER_OF_CELLS; i++) {
+    let rowCell = document.createElement('div')
+    rowCell.className = "row-cell"
+    for (let j = 0; j < NUMBER_OF_CELLS; j++) {
+      let cell = document.createElement('div')
+      cell.className = "cell"
+      cell.id = setId(i,j)
+      rowCell.appendChild(cell)
+    }
+    board.appendChild(rowCell)
+  }
+  console.log("BOARD INITIALISED!")
+}
+
+function setId(i: number, j: number): string{
+  return i.toString() + "-" + j.toString()
+}
+
+function getId(id: string): string[]{
+  return id.split('-')
+}
+
+
+function setSnakeHead() {
+  let snakeStartRow = Math.floor(Math.random()*(NUMBER_OF_CELLS))
+  let snakeStartCol = Math.floor(Math.random()*(NUMBER_OF_CELLS))
+  console.log(setId(snakeStartRow, snakeStartCol))
+  let startCell = document.getElementById(setId(snakeStartRow, snakeStartCol))
+  if(startCell != null) startCell.style.background = "green"
+}
+// console.log(board?.ATTRIBUTE_NODE)
+
